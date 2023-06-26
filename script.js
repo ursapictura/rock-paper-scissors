b91caff9eca436728d3279d184aaced6cf0a8b25
ursapictura <hsmithfreelance@gmail.com>
2023-06-25 19:51:49 -0500

    function getComputerChoice created and working

---
+++ script.js
@@ -0,0 +1,25 @@
+function getComputerChoice () {
+    let x = Math.floor(Math.random() * 10)
+        console.log(x);
+    if (x <= 3) {
+        console.log("Rock");
+    }
+    else if (x > 3 && x < 7) {
+        console.log("Paper");
+    }
+    else if (x >= 7) {
+        console.log("Scissors")
+    }
+}
+
+/* create function for computer response */
+
+/* randomize computer response using Math.random */
+
+/* assign if/else values to Math.random return */
+
+/* request user input Rock, Paper, or Scissors */
+
+/* compare input against computer response */
+
+/* create hierarchy for responses */
